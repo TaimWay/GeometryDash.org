652d25d5d3cb6e1b16ae1c8f8a24d4f55d87aa13
TaimWay <taimway@gmail.com>
2025-12-28 10:46:10 +0800

    Modify the main content of the page, added more details

--- src/views/Game/online-level.vue
+++ src/views/Game/online-level.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+    <div class="list-page">
+        <OnlineLevel></OnlineLevel>
+    </div>
+</template>
+
 <script setup>
-    </script>
+import OnlineLevel from '../../components/WinList.vue'
+</script>
+
+<style scoped>
+.list-page {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    padding: 10px;
+    background-image: linear-gradient(#0065FD, #002E73);
+    display: flex;
+}
+</style>
